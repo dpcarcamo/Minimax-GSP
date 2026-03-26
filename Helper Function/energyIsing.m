0ae371fce1482d984112de3acc44c242342ea263
David Carcamo <39925934+dpcarcamo@users.noreply.github.com>
2026-03-26 15:30:16 -0400

    Delete Helper Function directory

--- Helper Function/energyIsing.m
+++
@@ -1,7 +0,0 @@
-function Emean = energyIsing(spin, J, h)
-    %ENERGYISING Mean energy per spin.
-    %   Emean = ENERGYISING(spin, J) returns the mean energy per spin of the
-    %   configuration |spin|. |spin| is a matrix of +/- 1's. |J| is a scalar.
-
-    Emean = -(1./2).*(spin.' * J * spin)  - h.'*spin;
-end
